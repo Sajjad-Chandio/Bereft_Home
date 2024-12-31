cf9adb589c41d55741b5ecdef23056ab2196d9b6
LunyCipres <sarahnoor12232@gmail.com>
2025-01-01 01:48:35 +0500

    main menu complete

--- Assets/Scripts/UI/LinkHandler.cs
+++ Assets/Scripts/UI/LinkHandler.cs
@@ -2,31 +2,12 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
 
-public class LinkHandler : MonoBehaviour, IPointerClickHandler
+public class LinkHandler : MonoBehaviour
 {
-    public TextMeshProUGUI textMeshPro;
-
-    void Start()
+    public string url;
+    public void OpenURL()
     {
-        if (textMeshPro == null)
-        {
-            textMeshPro = GetComponent<TextMeshProUGUI>();
-        }
-    }
-
-    // Called when the UI element is clicked
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        // Get the index of the clicked link
-        int linkIndex = TMP_TextUtilities.FindIntersectingLink(textMeshPro, eventData.position, Camera.main);
-
-        if (linkIndex != -1) // If a link was clicked
-        {
-            TMP_LinkInfo linkInfo = textMeshPro.textInfo.linkInfo[linkIndex];
-            string url = linkInfo.GetLinkID(); // Get the link's URL
-
-            Application.OpenURL(url); // Open the link in the default browser
-            Debug.Log($"Opening link: {url}");
-        }
+        Application.OpenURL(url);
+        Debug.Log($"Opening URL: {url}"); 
     }
 }
