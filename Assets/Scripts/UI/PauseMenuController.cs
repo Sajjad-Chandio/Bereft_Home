56715137584146c4ce6ee1a24b6d7ce15e6d6169
LunyCipres <sarahnoor12232@gmail.com>
2025-01-01 15:31:19 +0500

    main level changes
    Sajjad's work pass #3

---
+++ Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public GameObject pauseMenuPanel;
+    public AudioSource[] audioSources;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        if (audioSources.Length == 0)
+        {
+            audioSources = FindObjectsOfType<AudioSource>();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+        pauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f;
+
+        foreach (AudioSource audio in audioSources)
+        {
+            audio.Pause();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f; 
+        
+        foreach (AudioSource audio in audioSources)
+        {
+            audio.UnPause();
+        }
+    }
+
+    public void QuitToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+        Debug.Log("Game Quit!");
+    }
+}
